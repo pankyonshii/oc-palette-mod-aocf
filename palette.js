54d33d0dcfca6b2e80053728c99a377f775f7936
pan <39769546+pankyonshii@users.noreply.github.com>
2022-05-01 22:50:29 -0400

    what makes the mod work

---
+++ palette.js
@@ -0,0 +1,15 @@
+{
+  "console": false,
+  "dat_dump": false,
+  "patches": [
+    {
+      "archive": "repos/nmlgc/base_tsa/"
+    },
+    {
+      "archive": "repos/thpatch/palette"
+    },
+    {
+      "archive": "repos/nmlgc/base_tasofro/"
+    }
+  ]
+}
